2d1ad299ce3b475c365a83c15be3da875c21765b
RacHiT101 <rachitgala05@gmail.com>
2023-05-18 17:16:53 +0530

    deploying

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import First from "./pages/First";
 import Second from "./pages/Second";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<First />} />
-          <Route path="/second" element={<Second/>} />
+          <Route path="/second" element={<Second />} />
         </Routes>
       </Router>
     </>
